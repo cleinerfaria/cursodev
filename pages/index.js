a471037346b100414254ff22216f383a7bc5efab
cleinerfaria <160253932+cleinerfaria@users.noreply.github.com>
2024-09-01 09:59:08 +0000

    configurado o `prettier`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 function Home() {
-    return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <h1>Thaís, vamos sair para jantar hoje?</h1>
-  </div>
-  
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <h1>Thaís, vamos sair para jantar hoje?</h1>
+    </div>
+  );
 }
 
 export default Home;
