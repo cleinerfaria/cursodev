24b5032c4dc7b3938b57ab8c246d6e2484174b8e
Cleiner Faria <cleinerfaria@gmail.com>
2024-09-05 15:27:22 -0300

    acrecentado logo no `index.js`

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,23 @@ function Home() {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
+        backgroundColor: "#000",
+        margin: "0",
+        padding: "0",
       }}
     >
-      <h1>Felipe, Lucas e Fábio, o primeiro passo já foi dado!</h1>
+      <img
+        src="/assets/images/logo/logo_white.png"
+        alt="Logomarca Cleiner Faria"
+        style={{
+          width: "300px",
+          height: "auto",
+          filter: "drop-shadow(0 0 20px rgba(0, 0, 255, 0.9)) drop-shadow(0 0 40px rgba(255, 0, 0, 0.3))",
+        }}
+      />
     </div>
   );
 }
 
 export default Home;
+
