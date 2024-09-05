48701a9cb8342b1e5202820d99cf0de066e860dc
Cleiner Faria <cleinerfaria@gmail.com>
2024-09-05 17:34:27 -0300

    testes correram perfeitamente

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,5 @@
 function status(req, res) {
-  res.status(200).json({ chave: "alunos ssão acima da média" });
+  res.status(200).json({ chave: "alunos são acima da média" });
 }
 
 export default status;
