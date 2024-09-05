1aacab383251d2396652ec53175847533fa3cba8
Cleiner Faria <cleinerfaria@gmail.com>
2024-09-05 15:42:58 -0300

    mensagem na `index`

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ function Home() {
     <div
       style={{
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
@@ -15,14 +16,25 @@ function Home() {
         src="/assets/images/logo/logo_white.png"
         alt="Logomarca Cleiner Faria"
         style={{
-          width: "300px",
+          width: "250px",
           height: "auto",
-          filter: "drop-shadow(0 0 20px rgba(0, 0, 255, 0.9)) drop-shadow(0 0 40px rgba(255, 0, 0, 0.3))",
+          filter:
+            "drop-shadow(0 0 20px rgba(0, 0, 255, 0.9)) drop-shadow(0 0 40px rgba(255, 0, 0, 0.3))",
         }}
       />
+      <span
+        style={{
+          color: "#fff", // Cor neon/tecnológica
+          marginTop: "20px",
+          fontSize: "14px", // Tamanho maior para modernizar
+          fontFamily: "'Orbitron', sans-serif", // Fonte moderna e tecnológica
+          letterSpacing: "2px", // Espaçamento entre letras para um efeito mais "futurista"
+        }}
+      >
+        Vamos dominar o mundo!!!
+      </span>
     </div>
   );
 }
 
 export default Home;
-
