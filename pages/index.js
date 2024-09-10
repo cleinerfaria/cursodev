1bcbaa7df4d55bfd4c63aa9dc80efa2314d941b3
Cleiner Faria <cleinerfaria@gmail.com>
2024-09-10 20:12:24 -0300

    agora com tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,15 @@
-function Home() {
+export default function Home() {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "#000",
-        margin: "0",
-        padding: "0",
-      }}
-    >
+    <div className="flex flex-col justify-center items-center h-screen bg-black m-0 p-0">
       <img
         src="/assets/images/logo/logo_white.png"
         alt="Logomarca Cleiner Faria"
-        style={{
-          width: "250px",
-          height: "auto",
-          filter:
-            "drop-shadow(0 0 20px rgba(0, 0, 255, 0.9)) drop-shadow(0 0 40px rgba(255, 0, 0, 0.3))",
-        }}
+        className="w-[250px] h-auto drop-shadow-[0_0_40px_rgba(255,0,0,0.3)]"
       />
-      <span
-        style={{
-          color: "#fff", // Cor neon/tecnológica
-          marginTop: "20px",
-          fontSize: "14px", // Tamanho maior para modernizar
-          fontFamily: "'Orbitron', sans-serif", // Fonte moderna e tecnológica
-          letterSpacing: "2px", // Espaçamento entre letras para um efeito mais "futurista"
-        }}
-      >
-        STAGING!!!
+      <span className="text-white mt-5 text-sm font-orbitron tracking-wider">
+        Em breve um sistema que irá mudar a forma que a sua empresa consome
+        dados!!!
       </span>
     </div>
   );
 }
-
-export default Home;
