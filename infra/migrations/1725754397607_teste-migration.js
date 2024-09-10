efa1b3ea5b138ab28075b10ef9bd47d696aa633d
Cleiner Faria <cleinerfaria@gmail.com>
2024-09-10 19:45:13 -0300

    criado ramificação apra a base do `flow`

--- infra/migrations/1725754397607_teste-migration.js
+++ infra/migrations/1725754397607_teste-migration.js
@@ -2,6 +2,37 @@
 
 exports.shorthands = undefined;
 
-exports.up = (pgm) => {};
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "serial",
+      primaryKey: true,
+    },
+    username: {
+      type: "varchar(100)",
+      notNull: true,
+    },
+    email: {
+      type: "varchar(100)",
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: "varchar(255)",
+      notNull: true,
+    },
+    created_at: {
+      type: "timestamp",
+      default: pgm.func("current_timestamp"),
+    },
+    updated_at: {
+      type: "timestamp",
+      default: pgm.func("current_timestamp"),
+      onUpdate: pgm.func("current_timestamp"),
+    },
+  });
+};
 
-exports.down = (pgm) => {};
+exports.down = (pgm) => {
+  pgm.dropTable("users");
+};
