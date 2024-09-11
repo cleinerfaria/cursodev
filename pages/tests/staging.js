8ad94c04efb4b5c88027077011e844bec16a2b92
Cleiner Faria VeC <cleinerfaria@gmail.com>
2024-09-11 12:04:04 -0300

    ajustes no staging.js

--- pages/tests/staging.js
+++ pages/tests/staging.js
@@ -70,8 +70,47 @@ const categories = [
 export default function Example() {
   return (
     <div className='grid'>
+      <div className="flex w-full justify-center pt-24 px-4 !bg-black">
+        <div className="w-full max-w-md">
+          <TabGroup>
+            <TabList className="flex gap-4">
+              {categories.map(({ name }) => (
+                <Tab
+                  key={name}
+                  className="rounded-full py-1 px-3 text-sm/6 font-semibold text-white focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
+                >
+                  {name}
+                </Tab>
+              ))}
+            </TabList>
+            <TabPanels className="mt-3">
+              {categories.map(({ name, posts }) => (
+                <TabPanel key={name} className="rounded-xl bg-white/5 p-3">
+                  <ul>
+                    {posts.map((post) => (
+                      <li key={post.id} className="relative rounded-md p-3 text-sm/6 transition hover:bg-white/5">
+                        <a href="#" className="font-semibold text-white">
+                          <span className="absolute inset-0" />
+                          {post.title}
+                        </a>
+                        <ul className="flex gap-2 text-white/50" aria-hidden="true">
+                          <li>{post.date}</li>
+                          <li aria-hidden="true">&middot;</li>
+                          <li>{post.commentCount} comments</li>
+                          <li aria-hidden="true">&middot;</li>
+                          <li>{post.shareCount} shares</li>
+                        </ul>
+                      </li>
+                    ))}
+                  </ul>
+                </TabPanel>
+              ))}
+            </TabPanels>
+          </TabGroup>
+        </div>
+      </div>
       <div className="flex h-screen w-full justify-center pt-24 px-4 !bg-black">
-        <div className="fixed top-24 w-52 text-right">
+        <div className="w-full max-w-md">
           <Menu __demoMode>
             <MenuButton className="inline-flex items-center gap-2 rounded-md bg-gray-800 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-700 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white">
               Options
@@ -114,45 +153,6 @@ export default function Example() {
               </MenuItem>
             </MenuItems>
           </Menu>
-        </div>
-      </div>
-      <div className="flex h-screen w-full justify-center pt-24 px-4 !bg-black">
-        <div className="w-full max-w-md">
-          <TabGroup>
-            <TabList className="flex gap-4">
-              {categories.map(({ name }) => (
-                <Tab
-                  key={name}
-                  className="rounded-full py-1 px-3 text-sm/6 font-semibold text-white focus:outline-none data-[selected]:bg-white/10 data-[hover]:bg-white/5 data-[selected]:data-[hover]:bg-white/10 data-[focus]:outline-1 data-[focus]:outline-white"
-                >
-                  {name}
-                </Tab>
-              ))}
-            </TabList>
-            <TabPanels className="mt-3">
-              {categories.map(({ name, posts }) => (
-                <TabPanel key={name} className="rounded-xl bg-white/5 p-3">
-                  <ul>
-                    {posts.map((post) => (
-                      <li key={post.id} className="relative rounded-md p-3 text-sm/6 transition hover:bg-white/5">
-                        <a href="#" className="font-semibold text-white">
-                          <span className="absolute inset-0" />
-                          {post.title}
-                        </a>
-                        <ul className="flex gap-2 text-white/50" aria-hidden="true">
-                          <li>{post.date}</li>
-                          <li aria-hidden="true">&middot;</li>
-                          <li>{post.commentCount} comments</li>
-                          <li aria-hidden="true">&middot;</li>
-                          <li>{post.shareCount} shares</li>
-                        </ul>
-                      </li>
-                    ))}
-                  </ul>
-                </TabPanel>
-              ))}
-            </TabPanels>
-          </TabGroup>
         </div>
       </div>
     </div>
